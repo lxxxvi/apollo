51189eb6e0fb2e3880cfe00f84ba6888f26fea82
lxxxvi <lxxxvi@users.noreply.github.com>
2018-09-23 17:14:28 +0200

    Create poll with system test

--- test/system/polls_test.rb
+++ test/system/polls_test.rb
@@ -14,6 +14,33 @@ class PollsTest < ApplicationSystemTestCase
     find_label_and_textarea_for('poll_description')
   end
 
+  test 'submit empty poll form' do
+    visit new_poll_path
+
+    within('form#new_poll') do
+      click_on('Create Poll')
+    end
+
+    assert_selector 'h1', text: 'New poll'
+
+    assert_selector('form#new_poll') do
+      assert_equal 2, all('input[aria-invalid="true"]').count
+    end
+  end
+
+  test 'submit complete poll form' do
+    visit new_poll_path
+
+    within('form#new_poll') do
+      fill_in('Title', with: 'Best singer')
+      fill_in('Email', with: 'best-singer@apollo.test')
+      fill_in('Description', with: 'Lorem ipsum')
+      click_on('Create Poll')
+    end
+
+    assert_selector 'h1', text: 'Best singer'
+  end
+
   private
 
   def find_label_and_input_for(html_id)
