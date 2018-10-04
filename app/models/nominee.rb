a08354075a8255473e1c8e00d9e1fd61f74edf72
mario <github@lxxxvi.ch>
2018-10-04 14:33:29 +0200

    Set custom_id on before creating nominee

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,3 +1,15 @@
 class Nominee < ApplicationRecord
+  before_create :create_custom_id
+
   belongs_to :poll
+
+  def to_param
+    custom_id
+  end
+
+  private
+
+  def create_custom_id
+    self.custom_id ||= SecureRandom.alphanumeric(12)
+  end
 end
