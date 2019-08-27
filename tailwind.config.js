2f8f8d3be7d764838cc4fca0bec12228e04fa89a
lxxxvi <lxxxvi@users.noreply.github.com>
2019-08-27 15:27:11 +0200

    introduce apollo-pink

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'apollo-pink': {
+          '100': '#fbebf4',
+          '200': '#f4b9da',
+          '300': '#e974b6',
+          '400': '#e974b6',
+          '500': '#e03997',
+          '600': '#b32d79',
+          '700': '#9c276a',
+          '800': '#701c4c',
+          '900': '#43112d',
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
