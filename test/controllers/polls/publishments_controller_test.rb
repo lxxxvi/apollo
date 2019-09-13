0461849072df8d64824e45b21b0825e2fcbb795a
mario <github@lxxxvi.ch>
2019-09-13 06:52:03 +0200

    add polls/publishments_controller test

---
+++ test/controllers/polls/publishments_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class Polls::PublishmentsControllerTest < ActionDispatch::IntegrationTest
+  attr_reader :draft_poll, :published_poll
+
+  setup do
+    @draft_poll = polls(:best_actress_draft)
+    @published_poll = polls(:best_actor_published)
+  end
+
+  test 'admin publishes a poll' do
+    sign_in_as(:julia_roberts)
+
+    poll = draft_poll
+
+    assert_changes -> { poll.state }, from: :draft, to: :published do
+      post poll_publishment_path(poll)
+      follow_redirect!
+      assert_response :success
+      poll.reload
+    end
+
+    assert_equal 'Poll published', flash[:notice]
+  end
+
+  test 'admin cannot publish unpublishable poll' do
+    sign_in_as(:julia_roberts)
+
+    poll = published_poll
+
+    assert_no_changes -> { poll.state } do
+      post poll_publishment_path(poll)
+      follow_redirect!
+      assert_response :success
+      poll.reload
+    end
+
+    assert_equal 'Poll is not publishable', flash[:error]
+  end
+
+  test 'non-admin cannot publish a poll' do
+    sign_in_as(:tina_fey)
+
+    poll = draft_poll
+
+    assert_no_changes -> { poll.state } do
+      assert_raise(Pundit::NotAuthorizedError) { post poll_publishment_path(poll) }
+    end
+  end
+
+  test 'guest cannot publish a poll' do
+    poll = draft_poll
+
+    assert_no_changes -> { poll.state } do
+      assert_raise(Pundit::NotAuthorizedError) { post poll_publishment_path(poll) }
+    end
+  end
+end
