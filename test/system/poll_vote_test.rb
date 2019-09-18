6f102551eae98b74b9ccc0b8093c458336c74104
lxxxvi <lxxxvi@users.noreply.github.com>
2019-09-18 10:18:55 +0200

    cast a vote
    closes #34

---
+++ test/system/poll_vote_test.rb
@@ -0,0 +1,15 @@
+require 'application_system_test_case'
+
+class PollsVoteTest < ApplicationSystemTestCase
+  test 'guest votes for a started poll using valid token' do
+    assert false, 'implement me'
+  end
+
+  test 'guest cannot vote for a poll using invalid token' do
+    assert false, 'implement me'
+  end
+
+  test 'guest cannot vote for a unstarted poll, with valid token' do
+    assert false, 'implement me'
+  end
+end
