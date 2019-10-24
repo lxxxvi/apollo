a06985065217f52d20319bca12ea3aacd21e3a71
lxxxvi <lxxxvi@users.noreply.github.com>
2019-10-24 14:44:54 +0200

    improve Admin::PollScheduleForm test

--- test/forms/admin/poll_schedule_form_test.rb
+++ test/forms/admin/poll_schedule_form_test.rb
@@ -14,9 +14,9 @@ class Admin::PollScheduleFormTest < ActiveSupport::TestCase
   end
 
   test 'closed_at_for_time_zone for new poll, no time zone, custom closed at' do
-    reference_date_time = DateTime.new(2019, 2, 2, 2, 2, 2)
+    params = to_date_time_params(:closed_at, 2019, 2, 2, 2, 2, 2)
 
-    form = Admin::PollScheduleForm.new(Poll.new, closed_at: reference_date_time)
+    form = Admin::PollScheduleForm.new(Poll.new, params)
 
     assert_equal 'UTC', form.time_zone
     assert_equal DateTime.new(2019, 2, 2, 2, 2, 2), form.closed_at
@@ -24,9 +24,10 @@ class Admin::PollScheduleFormTest < ActiveSupport::TestCase
   end
 
   test 'closed_at_for_time_zone for new poll, custom closed at' do
-    reference_date_time = DateTime.new(2019, 2, 2, 2, 2, 2)
+    closed_at_params = to_date_time_params(:closed_at, 2019, 2, 2, 2, 2, 2)
+    params = { time_zone: 'Berlin' }.merge(closed_at_params)
 
-    form = Admin::PollScheduleForm.new(Poll.new, time_zone: 'Berlin', closed_at: reference_date_time)
+    form = Admin::PollScheduleForm.new(Poll.new, params)
 
     assert_equal 'Berlin', form.time_zone
     assert_equal DateTime.new(2019, 2, 2, 2, 2, 2), form.closed_at
@@ -34,12 +35,22 @@ class Admin::PollScheduleFormTest < ActiveSupport::TestCase
   end
 
   test 'closed_at_for_time_zone for new poll, custom closed at, daylight saving time' do
-    reference_date_time = DateTime.new(2019, 10, 27, 1, 0)
+    closed_at_params = to_date_time_params(:closed_at, 2019, 10, 27, 1, 0)
+    params = { time_zone: 'Berlin' }.merge(closed_at_params)
 
-    form = Admin::PollScheduleForm.new(Poll.new, time_zone: 'Berlin', closed_at: reference_date_time)
+    form = Admin::PollScheduleForm.new(Poll.new, params)
 
     assert_equal 'Berlin', form.time_zone
     assert_equal DateTime.new(2019, 10, 27, 1, 0, 0), form.closed_at
     assert_equal DateTime.new(2019, 10, 26, 23, 0, 0, '+00'), form.closed_at_utc
+  end
+
+  private
+
+  def to_date_time_params(attribute_name, *date_time_fragments)
+    (1..6).map { |n| "#{attribute_name}(#{n}i)" }
+          .zip(date_time_fragments.map(&:to_s))
+          .keep_if { |_k, v| v.present? }
+          .to_h
   end
 end
