14192c51c624d1b8b4638aea9a6e31dafc45ecce
mario <github@lxxxvi.ch>
2019-11-04 17:03:48 +0100

    update mosaiq

--- app/services/image_placeholder.rb
+++ app/services/image_placeholder.rb
@@ -4,10 +4,10 @@ module ImagePlaceholder
   PARAMS = {
     width: 3,
     height: 3,
-    colors: %w[#79b1c0 #9d66ba #88cea6 #e03997 #ffffff]
+    palette: %w[#79b1c0 #9d66ba #88cea6 #e03997 #ffffff]
   }.freeze
 
   def base64
-    Mosaiq::Image.new(PARAMS).svg.base64
+    Mosaiq::Image.new(PARAMS).svg.to_base64
   end
 end
