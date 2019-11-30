4369033b542e6a352d1f926489ea0af736e91d73
lxxxvi <lxxxvi@users.noreply.github.com>
2019-11-30 10:07:04 +0100

    fix rubocop

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Apollo
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths << Rails.root.join('lib', 'error')
+    config.autoload_paths << Rails.root.join('lib/error')
 
     config.default_time_zone = ActiveSupport::TimeZone.new('UTC')
   end
